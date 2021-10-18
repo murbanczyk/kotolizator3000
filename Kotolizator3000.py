83b2bd0b6da78b5a24592377a3610850b2dae0b2
Mateusz Urbańczyk <murbanczyk@spektrino.com>
2021-10-18 14:58:48 +0200

    small fix

--- Kotolizator3000.py
+++ Kotolizator3000.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
 import analizatorkotow.guilib
-# from multiprocessing import freeze_support
-#
-# if __name__ == "__main__":
-#     freeze_support() #Keeps windows binary working for threading code
-#     analizatorkotow.guilib.mainGui()
-import sys
-from os.path import expanduser
 from multiprocessing import freeze_support
-# home = expanduser("~")
-# sys.stdout = open(home+'/.pies.log', 'w')
-# sys.stderr = open(home+'/.kot.log', 'w') #to prevent program from logging.
-if __name__ == "__main__":
-    freeze_support()
 
-    analizatorkotow.guilib.mainGui()
+if __name__ == "__main__":
+     freeze_support() #Keeps windows binary working for threading code
+     analizatorkotow.guilib.mainGui()
+
